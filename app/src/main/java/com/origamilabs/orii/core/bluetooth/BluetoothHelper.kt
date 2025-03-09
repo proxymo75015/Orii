88a4257c6dcc7f6c93b79233727e02e0f2e3f6dd
proxymo75015 <francois.simon75@gmail.com>
2025-03-09 20:01:36 +0100

    correction headsethandler

--- app/src/main/java/com/origamilabs/orii/core/bluetooth/BluetoothHelper.kt
+++ app/src/main/java/com/origamilabs/orii/core/bluetooth/BluetoothHelper.kt
@@ -3,30 +3,28 @@ package com.origamilabs.orii.core.bluetooth
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
-import android.util.Log
+import timber.log.Timber
 
 object BluetoothHelper {
-    private const val TAG = "BluetoothHelper"
-    private const val beginCvcAddress = "30:1F:9A:C0:00:00"
-    private const val beginPiTechAddress = "F4:45:ED:21:90:1F"
-    private const val beginSecondAddress = "00:02:5B:00:FF:01"
-    private const val endCvcAddress = "30:1F:9A:CF:FF:FF"
-    private const val endPiTechAddress = "F4:45:ED:21:90:82"
-    private const val endSecondAddress = "00:02:5B:00:FF:05"
 
     private var mBluetoothAdapter: BluetoothAdapter? = null
     private var mBluetoothManager: BluetoothManager? = null
+
+    private const val BEGIN_CVC_ADDRESS = "30:1F:9A:CF:FF:FF"
+    private const val END_CVC_ADDRESS = "30:1F:9A:CF:FF:FF"
+    private const val BEGIN_PI_TECH_ADDRESS = "F4:1F:9A:CF:FF:FF"
+    private const val END_PI_TECH_ADDRESS = "F4:1F:9A:CF:FF:FF"
+    private const val BEGIN_SECOND_ADDRESS = "00:02:5B:00:FF:01"
 
     /**
      * Vérifie si l'adresse MAC fournie se situe dans l'une des plages définies.
      */
     fun isOriiMacAddressInRange(address: String): Boolean {
-        return (address.compareTo(beginPiTechAddress, ignoreCase = true) >= 0 &&
-                address.compareTo(endPiTechAddress, ignoreCase = true) <= 0)
-                || (address.compareTo(beginSecondAddress, ignoreCase = true) >= 0 &&
-                    address.compareTo(endSecondAddress, ignoreCase = true) <= 0)
-                || (address.compareTo(beginCvcAddress, ignoreCase = true) >= 0 &&
-                    address.compareTo(endCvcAddress, ignoreCase = true) <= 0)
+        return (address.compareTo(BEGIN_PI_TECH_ADDRESS, ignoreCase = true) >= 0 &&
+                address.compareTo(END_PI_TECH_ADDRESS, ignoreCase = true) <= 0) ||
+                (address.compareTo(BEGIN_CVC_ADDRESS, ignoreCase = true) >= 0 &&
+                        address.compareTo(END_CVC_ADDRESS, ignoreCase = true) <= 0) ||
+                (address.compareTo(BEGIN_SECOND_ADDRESS, ignoreCase = true) == 0)
     }
 
     /**
@@ -34,14 +32,18 @@ object BluetoothHelper {
      */
     private fun initBluetoothAdapter(context: Context) {
         if (mBluetoothManager == null) {
-            mBluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as? BluetoothManager
+            mBluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager?
             if (mBluetoothManager == null) {
-                Log.e(TAG, "Unable to initialize BluetoothManager.")
+                Timber.e("Impossible d'obtenir le BluetoothManager.")
+                return
             }
         }
-        mBluetoothAdapter = mBluetoothManager?.adapter
+
         if (mBluetoothAdapter == null) {
-            Log.e(TAG, "Unable to obtain a BluetoothAdapter.")
+            mBluetoothAdapter = mBluetoothManager?.adapter
+            if (mBluetoothAdapter == null) {
+                Timber.e("Impossible d'obtenir le BluetoothAdapter.")
+            }
         }
     }
 
@@ -59,7 +61,7 @@ object BluetoothHelper {
      * Retourne le [BluetoothAdapter]. Si nécessaire, il est initialisé à partir du contexte.
      */
     fun getBluetoothAdapter(context: Context): BluetoothAdapter? {
-        if (mBluetoothManager == null || mBluetoothAdapter == null) {
+        if (mBluetoothAdapter == null) {
             initBluetoothAdapter(context)
         }
         return mBluetoothAdapter
